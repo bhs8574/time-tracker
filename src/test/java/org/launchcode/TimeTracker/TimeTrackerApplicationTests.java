3ef990815ed193ff7c0578b8cad302cdddeddca3
bhs8574 <bhs8574@gmail.com>
2020-08-16 14:42:28 -0500

    First Commit

---
+++ src/test/java/org/launchcode/TimeTracker/TimeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.TimeTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
