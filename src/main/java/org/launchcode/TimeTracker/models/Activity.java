55f3d1166527a5d77dd8988bef5533b5fc0908a0
bhs8574 <bhs8574@gmail.com>
2020-08-19 15:15:51 -0500

    Added a Date variable and a working flag to the Activity data entity to prep for implementing timer functionality.

--- src/main/java/org/launchcode/TimeTracker/models/Activity.java
+++ src/main/java/org/launchcode/TimeTracker/models/Activity.java
@@ -14,9 +14,15 @@ public class Activity extends AbstractEntity {
 
     private double hours;
 
+    private boolean working;
+
+    private Date workStarted;
+
     public Activity(String name, double hoursAccumulated, boolean working, Date workStarted) {
         this.name = name;
         this.hours = hoursAccumulated;
+        this.working = working;
+        this.workStarted = workStarted;
     }
 
     public Activity() {}
@@ -37,5 +43,28 @@ public class Activity extends AbstractEntity {
         this.hours = hoursAccumulated;
     }
 
+    public boolean isWorking() {
+        return working;
+    }
 
+    public void setWorking(boolean working) {
+        this.working = working;
+    }
+
+    public Date getWorkStarted() {
+        return workStarted;
+    }
+
+    public void setWorkStarted(Date workStarted) {
+        this.workStarted = workStarted;
+    }
+
+    public void endWork() {
+        if (! (this.workStarted == null)) {
+            Date timeNow = new Date();
+            double hoursToAdd = (timeNow.getTime() - this.workStarted.getTime())/(60.0*60.0*1000);
+            this.hours += hoursToAdd;
+            this.workStarted = null;
+        }
+    }
 }
